67790c73bd2529be78ab8cedad4794029aa057ba
Magnus Ellinge <magnus.ellinge@consid.se>
2022-04-01 00:44:07 +0200

    First checkin of custom export web component

---
+++ search-extensibility-samples/src/libraries/exportToCsvLibrary/loc/en-us.js
@@ -0,0 +1,11 @@
+define([], function () {
+    return {
+        ExportButtonText: "Export (csv)",
+        ExportInfoText: "Information",
+        ExportDialogHelpText: "Select if you want to export the current page/view or all the data (with a maximum of {maxhits} rows). Notice that there can be a difference in shown columns, value formatting and applied sorting in the outputted file compared to current view. There is also no guarantee that the exported content has included all potential hits.",
+        ExportBrowserNotSupportedText: "Browser is not supported. Please try again with another browser.",
+        ExportCurrentPageLabel: "Current page/view",
+        ExportAllLabel: "All (max {maxhits} rows)",
+        ExportDialogOKButtonText: "OK"
+    }
+});
---
+++ search-extensibility-samples/src/libraries/exportToCsvLibrary/loc/fr-fr.js
@@ -0,0 +1,11 @@
+define([], function () {
+    return {
+        ExportButtonText: "Exportation (csv)",
+        ExportInfoText: "Information",
+        ExportDialogHelpText: "Sélectionnez si vous souhaitez exporter la page/vue actuelle ou toutes les données (avec un maximum de {maxhits} lignes). Notez qu'il peut y avoir une différence dans les colonnes affichées, le formatage des valeurs et le tri appliqué dans le fichier généré par rapport à la vue actuelle. Il n'y a également aucune garantie que le contenu exporté a inclus tous les hits potentiels.",
+        ExportBrowserNotSupportedText: "Le navigateur n'est pas pris en charge. Veuillez réessayer avec un autre navigateur.",
+        ExportCurrentPageLabel: "Page/vue actuelle",
+        ExportAllLabel: "Tous (max {maxhits} lignes)",
+        ExportDialogOKButtonText: "OK"
+    }
+});
