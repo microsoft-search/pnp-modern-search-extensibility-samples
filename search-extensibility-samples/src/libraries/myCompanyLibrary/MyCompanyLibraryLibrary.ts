f029c5a4538f5109469233ca580f23c7586d0759
Franck Cornu <franck.cornu@ubisoft.com>
2022-09-13 11:30:14 -0400

    - Updated search extensibility module version + added sample of invokeCard action for adaptive cards.

--- search-extensibility-samples/src/libraries/myCompanyLibrary/MyCompanyLibraryLibrary.ts
+++ search-extensibility-samples/src/libraries/myCompanyLibrary/MyCompanyLibraryLibrary.ts
@@ -1,27 +1,58 @@
-import { ServiceKey } from "@microsoft/sp-core-library";
+import { ServiceKey, ServiceScope } from "@microsoft/sp-core-library";
+import { PageContext } from "@microsoft/sp-page-context";
+import { SPHttpClient, SPHttpClientResponse } from "@microsoft/sp-http";
 import {  IExtensibilityLibrary, 
           IComponentDefinition, 
           ISuggestionProviderDefinition, 
           ISuggestionProvider,
           ILayoutDefinition, 
           LayoutType, 
-          ILayout 
+          ILayout,
+          IAdaptiveCardAction,
+          LayoutRenderType
 } from "@pnp/modern-search-extensibility";
+import * as Handlebars from "handlebars";
 import { MyCustomComponentWebComponent } from "../CustomComponent";
 import { Customlayout } from "../CustomLayout";
 import { CustomSuggestionProvider } from "../CustomSuggestionProvider";
 
 export class MyCompanyLibraryLibrary implements IExtensibilityLibrary {
+  
+
+  public static readonly serviceKey: ServiceKey<MyCompanyLibraryLibrary> =
+  ServiceKey.create<MyCompanyLibraryLibrary>('SPFx:MyCustomLibraryComponent', MyCompanyLibraryLibrary);
+
+  private _spHttpClient: SPHttpClient;
+  private _pageContext: PageContext;
+  private _currentWebUrl: string;
+
+  constructor(serviceScope: ServiceScope) {
+    serviceScope.whenFinished(() => {
+      this._spHttpClient = serviceScope.consume(SPHttpClient.serviceKey);
+      this._pageContext = serviceScope.consume(PageContext.serviceKey);
+      this._currentWebUrl = this._pageContext.web.absoluteUrl;
+    });
+  }
 
   public getCustomLayouts(): ILayoutDefinition[] {
     return [
       {
-        name: 'PnP Custom layout',
+        name: 'PnP Custom layout (Handlebars)',
         iconName: 'Color',
-        key: 'CustomLayout',
+        key: 'CustomLayoutHandlebars',
         type: LayoutType.Results,
+        renderType: LayoutRenderType.Handlebars,
         templateContent: require('../custom-layout.html'),
-        serviceKey: ServiceKey.create<ILayout>('PnP:CustomLayout', Customlayout),
+        serviceKey: ServiceKey.create<ILayout>('PnP:CustomLayoutHandlebars', Customlayout),
+      },
+      {
+        name: 'PnP Custom layout (Adaptive Cards)',
+        iconName: 'Color',
+        key: 'CustomLayoutAdaptive',
+        type: LayoutType.Results,
+        renderType: LayoutRenderType.AdaptiveCards,
+        templateContent: JSON.stringify(require('../custom-layout.json'), null, "\t"),
+        serviceKey: ServiceKey.create<ILayout>('PnP:CustomLayoutAdaptive', Customlayout),
       }
     ];
   }
@@ -53,5 +84,42 @@ export class MyCompanyLibraryLibrary implements IExtensibilityLibrary {
     namespace.registerHelper('myHelper', (value: string) => {
       return new namespace.SafeString(value.toUpperCase());
     });
+  }
+
+  public invokeCardAction(action: any): void {
+    
+    // Process the action based on type
+    if (action.type == "Action.OpenUrl") {
+      window.open(action.url, "_blank");
+    } else if (action.type == "Action.Submit") {
+      // Process the action based on title
+      switch (action.title) {
+
+        case 'Click on item':
+
+           // Invoke the currentUser endpoing
+           this._spHttpClient.get(
+            `${this._currentWebUrl}/_api/web/currentUser`,
+            SPHttpClient.configurations.v1, 
+            null).then((response: SPHttpClientResponse) => {
+              response.json().then((json) => {
+                console.log(JSON.stringify(json));
+              });
+            });
+
+          break;
+
+        case 'Global click':
+          alert(JSON.stringify(action.data));
+          break;
+        default:
+          console.log('Action not supported!');
+          break;
+      }
+    }
+  }
+
+  public name(): string {
+    return 'MyCustomLibraryComponent';
   }
 }
