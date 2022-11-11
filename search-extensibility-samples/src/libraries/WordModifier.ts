03622e7e6f0f06599f7203dceae068f2bc758b74
edarroudi <935493+edarroudi@users.noreply.github.com>
2022-11-11 13:51:31 +0100

    updated interface changes

--- search-extensibility-samples/src/libraries/WordModifier.ts
+++ search-extensibility-samples/src/libraries/WordModifier.ts
@@ -1,4 +1,4 @@
-import { BaseQueryModifier, IDataContext } from "@pnp/modern-search-extensibility";
+import { BaseQueryModifier } from "@pnp/modern-search-extensibility";
 import { IPropertyPaneGroup, PropertyPaneTextField } from '@microsoft/sp-property-pane';
 import * as myLibraryStrings from 'MyCompanyLibraryLibraryStrings';
 
@@ -15,15 +15,15 @@ export class WordModifier extends BaseQueryModifier<IWordModifierProperties> {
     this._regex = new RegExp('\\b(?!OR|NEAR|ONEAR|WORDS|XRANK\\b)\\w+(?!(\\s)*[:,=,<,>]|\\.\\.)\\b', 'gm');
   }
 
-  public async modifyQuery(queryText: string, dataContext: IDataContext, resolveTokens: (string: string) => Promise<string>): Promise<string> {
+  public async modifyQuery(queryText: string): Promise<string> {
 
     this._regex.lastIndex = 0;
 
     const alteredQueryText = queryText?.replace(this._regex, (match => {
-      return  `${this.properties.prefix}${match}${this.properties.suffix}`;
+      return `${this.properties.prefix}${match}${this.properties.suffix}`;
     }));
 
-    return  alteredQueryText; 
+    return alteredQueryText;
   }
 
   public getPropertyPaneGroupsConfiguration(): IPropertyPaneGroup[] {
