94fbaa3223667fa48aae8cc5d2c550ce9db79b70
Erfan <935493+edarroudi@users.noreply.github.com>
2022-11-11 16:29:27 +0100

    Added QueryModifier extension sample
    * Added WordModifier sample as query modifier extension

--- search-extensibility-samples/src/libraries/myCompanyLibrary/loc/en-us.js
+++ search-extensibility-samples/src/libraries/myCompanyLibrary/loc/en-us.js
@@ -2,6 +2,15 @@ define([], function() {
   return {
     "PropertyPaneDescription": "Description",
     "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field"
+    "DescriptionFieldLabel": "Description Field",
+    "WordModifier": {
+      "GroupName":"WordModifier Settings",
+      "PrefixLabel":"Prefix Text",
+      "PrefixDescription":"A string to prepend to each word found in the input query text.",
+      "PrefixPlaceholder":"*",
+      "SuffixLabel":"Suffix Text",
+      "SuffixDescription":"A string to append to each word found in the input query text.",
+      "SuffixPlaceholder":"*"
+    }
   }
 });
